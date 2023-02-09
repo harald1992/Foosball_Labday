c176ad328c7170b7783ba454f15fe0066955af5f
Ryan Kool <ryankool@Ryans-MBP.corp.capgemini.com>
2023-02-09 11:51:49 +0100

    fixing it

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,22 +1,67 @@
 import styles from "./Avatar.module.css";
-import avatar from "../../assets/avatar.jpeg";
 import edit from "../../assets/edit.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ImageUploading from "react-images-uploading";
 
 export const Avatar = ({ className }) => {
     const [selectedImage, setSelectedImage] = useState(null);
+    const maxNumber = 69;
 
-    const handleEditClick = () => {
-        console.log("edit clicked");
+    const onChange = (imageList) => {
+        // data for submit
+        debugger;
+        setSelectedImage(imageList[0].data_url);
+        localStorage.setItem("avatar", imageList[0].data_url);
     };
+
+    useEffect(() => {
+        const avatar = localStorage.getItem("avatar");
+        if (avatar) {
+            setSelectedImage(avatar);
+        }
+    }, []);
+
     return (
-        <div className={`${styles.avatar} ${className ? className : ""}`}>
-            <div className={styles["avatar-image"]}>
-                <img className={styles.image} src={avatar} />
-            </div>
-            <button className={styles.edit} onClick={handleEditClick}>
-                <img className={styles["edit-image"]} src={edit} />
-            </button>
-        </div>
+        <ImageUploading
+            multiple
+            value={selectedImage}
+            onChange={onChange}
+            onImageUpdate={onChange}
+            maxNumber={1}
+            dataURLKey="data_url"
+        >
+            {({ onImageUpload, onImageUpdate }) => (
+                <div
+                    className={`${styles.avatar} ${className ? className : ""}`}
+                >
+                    <div className={styles["avatar-image"]}>
+                        {selectedImage ? (
+                            <div className="image-item">
+                                <img
+                                    src={selectedImage}
+                                    alt=""
+                                    className={styles.image}
+                                />
+                            </div>
+                        ) : (
+                            <div className={styles.placeholder}></div>
+                        )}
+                    </div>
+                    {selectedImage ? (
+                        <img
+                            className={styles["edit-image"]}
+                            src={edit}
+                            onClick={onImageUpdate[0]}
+                        />
+                    ) : (
+                        <img
+                            className={styles["edit-image"]}
+                            src={edit}
+                            onClick={onImageUpload}
+                        />
+                    )}
+                </div>
+            )}
+        </ImageUploading>
     );
 };
