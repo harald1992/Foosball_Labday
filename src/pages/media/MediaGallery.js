a376d4b97f443d97440ff343a658839d0ad22af7
olafvanv <olafvanv@gmail.com>
2023-02-09 13:28:38 +0100

    localstorage for media

--- src/pages/media/MediaGallery.js
+++ src/pages/media/MediaGallery.js
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './MediaGallery.module.css';
 
 export const MediaGallery = () => {
   const [files, setFiles] = useState([]);
   const [url, setUrl] = useState('');
+  
+  useEffect(() => {
+    const images = JSON.parse(localStorage.getItem('images'));
+    if(images) setFiles(images);
+  }, [])
 
-  function handleChange(e) {
+  const onUpload = (e) => {
     const id = files.length + 1;
-    setFiles([
-      ...files,
-      { url: URL.createObjectURL(e.target.files[0]), title: 'Shame', id }
-    ]);
+    const file = { url: URL.createObjectURL(e.target.files[0]), title: 'Shame', id }
+
+    setFiles((files) => {
+      const updatedFiles = [...files, file];
+      localStorage.setItem('images', JSON.stringify(updatedFiles));
+      return updatedFiles;
+    });
+    
     setUrl('');
   }
 
@@ -21,7 +30,7 @@ export const MediaGallery = () => {
           files.map((media, i) => <MediaGalleryItem key={media.id} url={media.url} title={media.title} />)
         }
       </div>
-      <MediaGalleryUpload onChange={handleChange} url={url} />
+      <MediaGalleryUpload onUpload={onUpload} url={url} />
     </div>
   );
 }
@@ -46,7 +55,7 @@ function MediaGalleryUpload(props) {
   return (
     <div className={styles.upload}>
       <button onClick={onUploadClick}>Add Image</button>
-      <input id="file-input" type="file" onChange={props.onChange} value={props.url} hidden/>
+      <input id="file-input" type="file" onChange={props.onUpload} value={props.url} hidden/>
     </div>
   )
 }
