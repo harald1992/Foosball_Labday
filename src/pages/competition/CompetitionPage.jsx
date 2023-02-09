413ecabbd310efce1cdd8ab1a0ee8f91eb77f38f
Ryan Kool <ryankool@Ryans-MBP.corp.capgemini.com>
2023-02-09 14:32:02 +0100

    added competition page

--- src/pages/competition/CompetitionPage.jsx
+++ src/pages/competition/CompetitionPage.jsx
@@ -1,16 +1,51 @@
-import { Avatar } from "../../components/avatar/Avatar";
 import { Byline } from "../../components/byline/Byline";
 import styles from "./CompetitionPage.module.css";
 
 export const CompetitionPage = () => {
     return (
-        <div className={styles.avatarpage}>
-            competititon
-            <Byline
-                className={styles.byline}
-                storeName="byline"
-                bylineText="cool byline"
-            />
+        <div className={styles.competitionpage}>
+            <h1>competition</h1>
+            <div className={`${styles.toplevel} ${styles.level}`}>
+                <Byline
+                    className={styles.byline}
+                    storeName="round4team1"
+                    bylineText="winner semifinals"
+                />
+                <span className={styles.divider}>-</span>
+                <Byline
+                    className={styles.byline}
+                    storeName="round4team2"
+                    bylineText="winner semifinals"
+                />
+            </div>
+            <div className={`${styles.secondlevel} ${styles.level}`}>
+                <div className={styles.toplevel}>
+                    <Byline
+                        className={styles.byline}
+                        storeName="round3team1"
+                        bylineText="winner quaterfinals"
+                    />
+                    <span className={styles.divider}>-</span>
+                    <Byline
+                        className={styles.byline}
+                        storeName="round3team2"
+                        bylineText="winner quaterfinals"
+                    />
+                </div>
+                <div className={styles.toplevel}>
+                    <Byline
+                        className={styles.byline}
+                        storeName="round3team3"
+                        bylineText="winner quaterfinals"
+                    />
+                    <span className={styles.divider}>-</span>
+                    <Byline
+                        className={styles.byline}
+                        storeName="round3team4"
+                        bylineText="winner quaterfinals"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
