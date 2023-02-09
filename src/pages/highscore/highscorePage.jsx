40e12921d06a01e7fc00ae3da0e63b94eb1d0246
Ryan Kool <ryankool@Ryans-MBP.corp.capgemini.com>
2023-02-09 16:05:47 +0100

    fixed

--- src/pages/highscore/highscorePage.jsx
+++ src/pages/highscore/highscorePage.jsx
@@ -1,50 +1,56 @@
+import React, { useState } from "react";
 import styles from "./highscorePage.module.css";
 
 export const HighscorePage = () => {
-  return (
-    <div className={styles.highscorepage}>
-      <h1>Highscore</h1>
-      <div className="highscore-list">
-        <ol>
-          <li>
-            <div>Team One</div>
-            <div>10-0</div>
-          </li>
-          <li>
-            <div>Team Two</div>
-            <div>9-8</div>
-          </li>
-          <li>
-            <div>Team Three</div>
-            <div>7-0</div>
-          </li>
-          <li>
-            <div>Team Four</div>
-            <div>6-6</div>
-          </li>
-          <li>
-            <div>Team Five</div>
-            <div>4-4</div>
-          </li>
-        </ol>
-      </div>
-    </div>
-  );
-};
+    const [highscores, setHighscores] = useState([
+        { team: "Team One", score: "10-0" },
+        { team: "Team Two", score: "9-8" },
+        { team: "Team Three", score: "7-0" },
+        { team: "Team Four", score: "6-6" },
+        { team: "Team Five", score: "4-4" },
+    ]);
 
-// export function Highscore() {
-//   return (
-//     <div>
-//       <h1>Highscore</h1>
-//       <div>
-//         <ol class="highscore-list">
-//           <li>Team Boe 10-0</li>
-//           <li>Team Schrik 10-6</li>
-//           <li>Team one 6-0</li>
-//           <li>Team A-team 4-4</li>
-//           <li>Team Bla 3-2</li>
-//         </ol>
-//       </div>
-//     </div>
-//   );
-// }
+    const [teamName, setTeamName] = useState("");
+    const [teamScore, setScore] = useState("");
+
+    const handleSubmitHighScore = () => {
+        setHighscores((highscores) => {
+            return [
+                ...highscores,
+                {
+                    team: teamName,
+                    score: teamScore,
+                },
+            ];
+        });
+    };
+
+    return (
+        <div className={styles.highscorepage}>
+            <h1>Highscore</h1>
+            <div className="highscore-list">
+                <ol>
+                    {highscores.map((highscore, index) => (
+                        <li key={index}>
+                            <div>{highscore.team}</div>
+                            <div>{highscore.score}</div>
+                        </li>
+                    ))}
+                </ol>
+            </div>
+
+            <h2>Add highscore</h2>
+            <label>TeamName</label>
+            <input
+                defaultValue={teamName}
+                onChange={(e) => setTeamName(e.target.value)}
+            />
+            <label>Score</label>
+            <input
+                defaultValue={teamScore}
+                onChange={(e) => setScore(e.target.value)}
+            />
+            <button onClick={handleSubmitHighScore}>Submit score</button>
+        </div>
+    );
+};
