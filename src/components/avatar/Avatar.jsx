65ce894d16b3d92f236373662166353e30146a84
Ryan Kool <ryankool@Ryans-MacBook-Pro.local>
2023-02-09 10:53:55 +0100

    first version avatar

---
+++ src/components/avatar/Avatar.jsx
@@ -0,0 +1,21 @@
+import styles from "./Avatar.module.css";
+import avatar from "../../assets/avatar.jpeg";
+import edit from "../../assets/edit.png";
+
+export const Avatar = ({ className }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleEditClick = () => {
+        console.log("edit clicked");
+    };
+    return (
+        <div className={`${styles.avatar} ${className ? className : ""}`}>
+            <div className={styles["avatar-image"]}>
+                <img className={styles.image} src={avatar} />
+            </div>
+            <button className={styles.edit} onClick={handleEditClick}>
+                <img className={styles["edit-image"]} src={edit} />
+            </button>
+        </div>
+    );
+};
