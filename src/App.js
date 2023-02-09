68368aa25db430b492ef8b6499a97cfc3a788e59
Hiemstra <harald.hiemstra@capgemini.com>
2023-02-09 10:31:45 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Reserve } from './pages/Reserve';
+import { Home } from './pages/Home';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <nav>
+        <ul class="navigation">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/reserve">Reserve</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reserve" element={<Reserve />} />
+      </Routes>
+    </>
+  )
 }
+
+
+// function App() {
+//   return (
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+
+//       <nav>
+//         <ul>
+//           <li><Link to="/">Home</Link></li>
+//           <li><Link to="/books">Books</Link></li>
+//         </ul>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/books" />
+
+//       </Routes>
+//   );
+// }
 
 export default App;
---
+++ src/pages/Reserve.js
@@ -0,0 +1,3 @@
+export function Reserve() {
+    return <h1>Reserve the foosball table!</h1>
+}
